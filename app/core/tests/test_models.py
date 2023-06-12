87a8bb5c21b0e8d33327bc74030b6294763a01ba
Jayita10 <jayitapramanik2018@gmail.com>
2023-06-12 19:37:08 +0530

    add email requirement

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -9,7 +9,7 @@ class ModelTests(TestCase):
     '''Test Models.'''
 
     def test_create_user_with_email_successful(self):
-        '''Test if creating a user with an email is successful.'''
+        '''Test that creating a user with an email is successful.'''
         email = 'test@example.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
@@ -21,7 +21,7 @@ class ModelTests(TestCase):
         self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normalized(self):
-        '''Tset if email is normalized for new users.'''
+        '''Tset that email is normalized for new users.'''
         sample_emails = [
             ['test1@EXAMPLE.COM', 'test1@example.com'],
             ['Test2@Example.com', 'Test2@example.com'],
@@ -32,3 +32,8 @@ class ModelTests(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        '''Test that creating a user without an email raises a ValueError.'''
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
